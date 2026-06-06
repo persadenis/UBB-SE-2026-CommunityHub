87f300143e3ca1c62d60792b41267bb1f035e915
persadenis <persadenis@gmail.com>
2026-06-06 23:36:03 +0300

    Enable Postgres timestamp compatibility

--- Events_GSS.Data/Database/AppDatabaseConfiguration.cs
+++ Events_GSS.Data/Database/AppDatabaseConfiguration.cs
@@ -24,6 +24,7 @@ public static class AppDatabaseConfiguration
         var usePostgres = IsPostgresProvider(provider) || IsPostgresUrl(resolvedConnectionString);
         if (usePostgres)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             options.UseNpgsql(ToNpgsqlConnectionString(resolvedConnectionString));
         }
         else
